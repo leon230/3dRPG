a6cba372d6ca7abbcacf2fdf8da2c291e33b6db9
Leon <homik.lukasz@gmail.com>
2017-08-16 22:41:07 +0200

    Player Controller script

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Camera camera;
+    public LayerMask movementMask;
+
+	// Use this for initialization
+	void Start () {
+        camera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                Debug.Log("Hit" + hit.collider.name + " " + hit.point);
+                //Move player
+
+
+                //Stop focusing any obejcts
+
+            }
+        }
+	}
+}
