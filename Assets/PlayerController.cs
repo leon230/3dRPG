e34475543c59585218a98ab9892f65b050317c2c
Leon <homik.lukasz@gmail.com>
2017-08-16 22:53:54 +0200

    Player Motor script

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,32 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
 
-    Camera camera;
     public LayerMask movementMask;
 
-	// Use this for initialization
-	void Start () {
+    Camera camera;
+    PlayerMotor playerMotor;
+
+    // Use this for initialization
+    void Start()
+    {
         camera = Camera.main;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButtonDown(0))
+        playerMotor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
         {
             Ray ray = camera.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if(Physics.Raycast(ray, out hit, 100, movementMask))
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
             {
-                Debug.Log("Hit" + hit.collider.name + " " + hit.point);
-                //Move player
+                playerMotor.MoveToPoint(hit.point);
 
 
                 //Stop focusing any obejcts
 
             }
         }
-	}
+    }
 }
